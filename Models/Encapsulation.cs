971eb94d142ba3d66eae3a37f931a4b3cb9cba82
s3limm <yavuzselimemrem906@gmail.com>
2022-09-17 17:56:28 +0300

    Initial commit

---
+++ Models/Encapsulation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E4.Encapsulation_Prop_Get_Set.Models
+{
+    public class Encapsulation
+    {
+
+        public Encapsulation()
+        {
+            int test = number1;
+            number1 = 10;
+        }
+
+        int _number;
+
+        public int number1
+        {
+
+
+            get
+            {
+                return _number; // get özelliği burada bir değeri çağırmak için kullanılıyor.   
+            }
+
+            set
+            {
+                _number = value; // set özelliği ise atadığımız değeri değiştirmek için kullanılıyor.
+            }
+
+        }
+
+    }
+}
